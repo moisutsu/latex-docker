27923347ece32e5fafa48bae36f836d1cdfb8dea
moisutsu <moisutsu@gmail.com>
2020-05-10 01:53:31 +0900

    modify compile.sh

--- compile.sh
+++ compile.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-docker run --rm -v $PWD:/workdir paperist/alpine-texlive-ja uplatex report.tex
-docker run --rm -v $PWD:/workdir paperist/alpine-texlive-ja dvipdfmx report.dvi
+for file in $( ls . | grep .tex$ ); do
+    TEX_FILE=`echo $file | sed "s/\.tex\$//"`
+    docker run --rm -v $PWD:/workdir paperist/alpine-texlive-ja uplatex $TEX_FILE.tex
+    docker run --rm -v $PWD:/workdir paperist/alpine-texlive-ja dvipdfmx $TEX_FILE.dvi
+    rm -f $TEX_FILE.aux $TEX_FILE.dvi $TEX_FILE.log
+    if [ -e $TEX_FILE.pdf ]; then
+        if [ ! -e build/ ]; then
+            mkdir build
+        fi
+        mv $TEX_FILE.pdf build/$TEX_FILE.pdf
+    fi
+done
