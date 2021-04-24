cd06e226e20a86424e001eda57692d785e207afd
moisutsu <moisutsu@gmail.com>
2021-04-24 09:20:48 +0900

    Update template

--- compile.sh
+++ compile.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-for file in $( ls . | grep .tex$ ); do
-    TEX_FILE=`echo $file | sed "s/\.tex\$//"`
-    docker run --rm -v $PWD:/workdir paperist/alpine-texlive-ja uplatex $TEX_FILE.tex
-    docker run --rm -v $PWD:/workdir paperist/alpine-texlive-ja dvipdfmx $TEX_FILE.dvi
-    rm -f $TEX_FILE.aux $TEX_FILE.dvi $TEX_FILE.log
-    if [ -e $TEX_FILE.pdf ]; then
-        if [ ! -e build/ ]; then
-            mkdir build
-        fi
-        mv $TEX_FILE.pdf build/$TEX_FILE.pdf
-    fi
-done
+export PATH=/Library/TeX/texbin/:$PATH
+platex -halt-on-error ${FILE}
+pbibtex ${FILE}
+platex -halt-on-error ${FILE}
+platex -halt-on-error ${FILE}
+dvipdfmx ${FILE}.dvi
